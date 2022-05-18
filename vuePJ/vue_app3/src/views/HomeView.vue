52cdfca8c505c79b76f6b7d334608e3ad80e1b1e
hyunboklee <gusqhrzkq@naver.com>
2022-05-18 17:02:04 +0900

    Feat : 2022-05-18 vue Router

---
+++ vuePJ/vue_app3/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">test</div>
+  <router-link to="/notice">notice</router-link>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
---
+++ vuePJ/vue_app3/src/views/NoticeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <textarea width="100%" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoView',
+}
+</script>
